7697cdcd8ef5d68ddc9ec6532f3ef339ea5fc63e
prasert <sornprasit.pr@hotmail.com>
2019-10-04 18:57:16 +0700

    add catagory manu

---
+++ SmartHotel360_client/Source/SmartHotel.Clients/SmartHotel.Clients/Models/RestaurantMenuItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartHotel.Clients.Core.Models
+{
+    public class RestaurantMenuItem
+    {
+        public int id { get; set; }
+        public decimal MunuPrice { get; set; }
+        public string ManuName { get; set; }
+        public string ManuComment { get; set; }
+        public string ManuImg { get; set; }
+    }
+}
--- SmartHotel360_client/Source/SmartHotel.Clients/SmartHotel.Clients/ViewModels/RestaurantViewModel.cs
+++ SmartHotel360_client/Source/SmartHotel.Clients/SmartHotel.Clients/ViewModels/RestaurantViewModel.cs
@@ -1,4 +1,7 @@
-﻿using SmartHotel.Clients.Core.ViewModels.Base;
+﻿
+using MvvmHelpers;
+using SmartHotel.Clients.Core.Models;
+using SmartHotel.Clients.Core.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +10,41 @@ namespace SmartHotel.Clients.Core.ViewModels
 {
     public class RestaurantViewModel : ViewModelBase
     {
+        private RestaurantMenuItem restaurantMenu;
+        private ObservableRangeCollection<RestaurantMenuItem> listMenu;
         public RestaurantViewModel()
         {
+            ListMenu = new ObservableRangeCollection<RestaurantMenuItem>()
+            {
+                new RestaurantMenuItem{ id = 0, ManuComment="testAdd" , ManuImg = "FoodImage" , ManuName = "Food Name", MunuPrice = 500 },
+                new RestaurantMenuItem{ id = 1, ManuComment="testAdd2" , ManuImg = "FoodImage" , ManuName = "Food Name2", MunuPrice = 400 },
+                new RestaurantMenuItem{ id = 2, ManuComment="testAdd3" , ManuImg = "FoodImage" , ManuName = "Food Name3", MunuPrice = 300 },
+                new RestaurantMenuItem{ id = 3, ManuComment="testAdd4" , ManuImg = "FoodImage" , ManuName = "Food Name4", MunuPrice = 200 },
+                new RestaurantMenuItem{ id = 4, ManuComment="testAdd5" , ManuImg = "FoodImage" , ManuName = "Food Name5", MunuPrice = 100 },
+                new RestaurantMenuItem{ id = 5, ManuComment="testAdd6" , ManuImg = "FoodImage" , ManuName = "Food Name6", MunuPrice = 50 }
 
+            };
+        }
+
+        public RestaurantMenuItem RestaurantMenu
+        {
+            get => restaurantMenu;
+            set
+            {
+                if(value != null)
+                {
+                    
+                }
+            }
+        
+           
+        
+        }
+
+        public ObservableRangeCollection<RestaurantMenuItem> ListMenu
+        {
+            get => listMenu;
+            set => SetProperty(ref listMenu, value);
         }
 
     }
