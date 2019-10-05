8e98573ac3a98ce29d836ff7d6bc40d057c03100
Natthaset <natthaset2538@hotmail.com>
2019-10-05 19:58:53 +0700

    Add show menu on maintenance app

--- SmartHotel360_maintenance/SmartHotel.Clients.Maintenance/SmartHotel.Clients.Maintenance/App.xaml.cs
+++ SmartHotel360_maintenance/SmartHotel.Clients.Maintenance/SmartHotel.Clients.Maintenance/App.xaml.cs
@@ -11,7 +11,7 @@ namespace SmartHotel.Clients.Maintenance
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new MainPage());
         }
 
         protected override void OnStart()
