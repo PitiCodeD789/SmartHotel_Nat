4208ae2d959a1e8b5873ffe70e6d82e8eb80b4ac
prasert <sornprasit.pr@hotmail.com>
2019-10-05 19:33:58 +0700

    modify model and serate Category

---
+++ SmartHotel360_client/Source/SmartHotel.Clients/SmartHotel.Clients/Services/Restaurant/RestaurantService.cs
@@ -0,0 +1,33 @@
+﻿using SmartHotel.Clients.Core.Extensions;
+using SmartHotel.Clients.Core.Models;
+using SmartHotel.Clients.Core.Services.Request;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHotel.Clients.Core.Services.Restaurant
+{
+    public class RestaurantService : IRestaurantService
+    {
+        readonly IRequestService requestService;
+        public RestaurantService()
+        {
+            requestService = new RequestService();
+        }
+
+        public Task<IEnumerable<MenuSearchResult>> GetMenusAsync(string token = "")
+        {
+            var builder = new UriBuilder(AppSettings.HotelsEndpoint);
+            int Hotelid = AppSettings.HotelId;
+            Hotelid = 11;
+
+
+            builder.AppendToPath($"roomservice/{Hotelid}/menus");
+
+            var uri = builder.ToString();
+
+            return requestService.GetAsync<IEnumerable<Models.MenuSearchResult>>(uri, token);
+        }
+    }
+}
